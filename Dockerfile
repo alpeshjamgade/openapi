463c64522593dcf31c8dc7dd26903f164435a4af
Alpesh_Jamgade <alpesh@tradelab.in>
2025-05-14 17:43:08 +0530

    updated routes prefix

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,11 @@ RUN mkdir /app
 COPY . /app
 RUN ls
 WORKDIR /app
-RUN CGO_ENABLED=0 go build -o gocms ./cmd/main.go
-RUN chmod +x gocms
+RUN CGO_ENABLED=0 go build -o openapi-client ./cmd/main.go
+RUN chmod +x openapi-client
+
+# Install migrate CLI
+RUN go install -ldflags="-s -w" github.com/golang-migrate/migrate/v4/cmd/migrate@latest
 
 FROM alpine:latest
 RUN mkdir /app
@@ -13,7 +16,10 @@ RUN mkdir /app
 ENV ELASTIC_APM_RECORDING=false
 ENV ELASTIC_APM_ACTIVE=false
 ENV ELASTIC_APM_ENVIRONMENT=test
-ENV ELASTIC_APM_SERVICE_NAME=gocms
+ENV ELASTIC_APM_SERVICE_NAME=openapi-client
 
-COPY --from=builder /app/gocms /app
-CMD [ "./app/gocms" ]
+COPY --from=builder /app/openapi-client /app
+COPY --from=builder /go/bin/migrate /usr/local/bin/
+COPY private /app/private
+
+CMD [ "./app/openapi-client" ]
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -30,49 +30,49 @@ func (h *Handler) SetupRoutes(r *mux.Router) {
 	admin.Use(middlewares.AuthenticateAdmin)
 
 	// user
-	user.HandleFunc("/api/v1/user", h.CreateUser).Methods(http.MethodPost)
-	user.HandleFunc("/api/v1/user", h.GetUserByEmail).Methods(http.MethodGet)
-	user.HandleFunc("/api/v1/user", h.UpdateUser).Methods(http.MethodPut)
-	user.HandleFunc("/api/v1/user", h.DeleteUserByEmail).Methods(http.MethodDelete)
+	user.HandleFunc("/api/openapi/v1/user", h.CreateUser).Methods(http.MethodPost)
+	user.HandleFunc("/api/openapi/v1/user", h.GetUserByEmail).Methods(http.MethodGet)
+	user.HandleFunc("/api/openapi/v1/user", h.UpdateUser).Methods(http.MethodPut)
+	user.HandleFunc("/api/openapi/v1/user", h.DeleteUserByEmail).Methods(http.MethodDelete)
 
 	// admin
-	admin.HandleFunc("/api/v1/admin/user", h.CreateAdmin).Methods(http.MethodPost)
-	admin.HandleFunc("/api/v1/admin/user", h.GetAdminByEmail).Methods(http.MethodGet)
-	admin.HandleFunc("/api/v1/admin/user", h.UpdateAdmin).Methods(http.MethodPut)
-	admin.HandleFunc("/api/v1/admin/user", h.DeleteAdminByEmail).Methods(http.MethodDelete)
+	admin.HandleFunc("/api/openapi/v1/admin/user", h.CreateAdmin).Methods(http.MethodPost)
+	admin.HandleFunc("/api/openapi/v1/admin/user", h.GetAdminByEmail).Methods(http.MethodGet)
+	admin.HandleFunc("/api/openapi/v1/admin/user", h.UpdateAdmin).Methods(http.MethodPut)
+	admin.HandleFunc("/api/openapi/v1/admin/user", h.DeleteAdminByEmail).Methods(http.MethodDelete)
 
 	// user_app
-	user.HandleFunc("/api/v1/user/app", h.CreateUserApp).Methods(http.MethodPost)
-	user.HandleFunc("/api/v1/user/app", h.GetUserAppByID).Methods(http.MethodGet)
-	user.HandleFunc("/api/v1/user/app", h.UpdateUserApp).Methods(http.MethodPut)
-	user.HandleFunc("/api/v1/user/app", h.DeleteUserAppByID).Methods(http.MethodDelete)
+	user.HandleFunc("/api/openapi/v1/user/app", h.CreateUserApp).Methods(http.MethodPost)
+	user.HandleFunc("/api/openapi/v1/user/app", h.GetUserAppByID).Methods(http.MethodGet)
+	user.HandleFunc("/api/openapi/v1/user/app", h.UpdateUserApp).Methods(http.MethodPut)
+	user.HandleFunc("/api/openapi/v1/user/app", h.DeleteUserAppByID).Methods(http.MethodDelete)
 
 	// plan
-	admin.HandleFunc("/api/v1/admin/plan", h.CreatePlan).Methods(http.MethodPost)
-	admin.HandleFunc("/api/v1/admin/plan", h.GetPlanByID).Methods(http.MethodGet)
-	admin.HandleFunc("/api/v1/admin/plan", h.UpdatePlan).Methods(http.MethodPut)
-	admin.HandleFunc("/api/v1/admin/plan", h.DeletePlanByID).Methods(http.MethodDelete)
+	admin.HandleFunc("/api/openapi/v1/admin/plan", h.CreatePlan).Methods(http.MethodPost)
+	admin.HandleFunc("/api/openapi/v1/admin/plan", h.GetPlanByID).Methods(http.MethodGet)
+	admin.HandleFunc("/api/openapi/v1/admin/plan", h.UpdatePlan).Methods(http.MethodPut)
+	admin.HandleFunc("/api/openapi/v1/admin/plan", h.DeletePlanByID).Methods(http.MethodDelete)
 
 	// auth
-	public.HandleFunc("/api/v1/user/login", h.Login).Methods(http.MethodPost)
-	user.HandleFunc("/api/v1/user/logout", h.Logout).Methods(http.MethodPost)
-	public.HandleFunc("/api/v1/admin/login", h.AdminLogin).Methods(http.MethodPost)
-	user.HandleFunc("/api/v1/admin/logout", h.AdminLogin).Methods(http.MethodPost)
+	public.HandleFunc("/api/openapi/v1/user/login", h.Login).Methods(http.MethodPost)
+	user.HandleFunc("/api/openapi/v1/user/logout", h.Logout).Methods(http.MethodPost)
+	public.HandleFunc("/api/openapi/v1/admin/login", h.AdminLogin).Methods(http.MethodPost)
+	user.HandleFunc("/api/openapi/v1/admin/logout", h.AdminLogin).Methods(http.MethodPost)
 
 	// role
-	admin.HandleFunc("/api/v1/admin/role", h.CreateRole).Methods(http.MethodPost)
-	admin.HandleFunc("/api/v1/admin/role", h.GetRoleByName).Methods(http.MethodGet)
-	admin.HandleFunc("/api/v1/admin/role", h.UpdateRole).Methods(http.MethodPut)
-	admin.HandleFunc("/api/v1/admin/role", h.DeleteRoleByName).Methods(http.MethodDelete)
+	admin.HandleFunc("/api/openapi/v1/admin/role", h.CreateRole).Methods(http.MethodPost)
+	admin.HandleFunc("/api/openapi/v1/admin/role", h.GetRoleByName).Methods(http.MethodGet)
+	admin.HandleFunc("/api/openapi/v1/admin/role", h.UpdateRole).Methods(http.MethodPut)
+	admin.HandleFunc("/api/openapi/v1/admin/role", h.DeleteRoleByName).Methods(http.MethodDelete)
 
 	// permission
-	admin.HandleFunc("/api/v1/admin/permission", h.CreatePermission).Methods(http.MethodPost)
-	admin.HandleFunc("/api/v1/admin/permission", h.GetPermissionByName).Methods(http.MethodGet)
-	admin.HandleFunc("/api/v1/admin/permission", h.UpdatePermission).Methods(http.MethodPut)
-	admin.HandleFunc("/api/v1/admin/permission", h.DeletePermissionByName).Methods(http.MethodDelete)
+	admin.HandleFunc("/api/openapi/v1/admin/permission", h.CreatePermission).Methods(http.MethodPost)
+	admin.HandleFunc("/api/openapi/v1/admin/permission", h.GetPermissionByName).Methods(http.MethodGet)
+	admin.HandleFunc("/api/openapi/v1/admin/permission", h.UpdatePermission).Methods(http.MethodPut)
+	admin.HandleFunc("/api/openapi/v1/admin/permission", h.DeletePermissionByName).Methods(http.MethodDelete)
 
 	// role permission
-	admin.HandleFunc("/api/v1/admin/role/permission", h.CreateRolePermission).Methods(http.MethodPost)
+	admin.HandleFunc("/api/openapi/v1/admin/role/permission", h.CreateRolePermission).Methods(http.MethodPost)
 
 }
 
